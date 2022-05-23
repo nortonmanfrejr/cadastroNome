0c81abd5ea837c4019b0448aaff9f8e1f635c8b0
Norton Domingues Manfre Filho <nfilho@al.casperlibero.edu.br>
2022-05-23 14:22:08 -0300

    Cadastro de Nomes

---
+++ src/main/java/com/cadastroCliente/crm/controller/clienteController.java
@@ -0,0 +1,38 @@
+package com.cadastroCliente.crm.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cadastroCliente.crm.cliente.model.Cliente;
+import com.cadastroCliente.crm.repository.ClienteRepository;
+
+
+@RestController
+@RequestMapping("/cliente")
+public class clienteController {
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@GetMapping
+	public List<Cliente> listar(){
+		
+		return clienteRepository.findAll();
+		
+	}
+	
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente adiciona(@RequestBody Cliente cliente) {
+		return clienteRepository.save(cliente);
+	}
+}
